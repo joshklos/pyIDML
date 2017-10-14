fa1e0b17cd715922ab1df008917bbc252c84307f
Josh Klos <me@joshklos.com>
2017-10-13 20:15:51 -0500

    Initial steps

---
+++ pyidml/__init__.py
@@ -0,0 +1,2 @@
+def generator():
+    print("Create New Document")
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyidml',
+      version='0.01',
+      description='Python library for reading and manipulating IDML and ICML files',
+      url='https://gitlab.com/joshklos/pyIDML/',
+      author='Josh Klos',
+      license='MIT',
+      packages=['pyidml'],
+      zip_safe=False
+)
