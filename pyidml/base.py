93304d2004f2db5e7f445ac4213108b0e6d61510
Josh Klos <me@joshklos.com>
2017-10-17 13:04:51 -0500

    Fixed some pep issues, and added some basic error handling.

--- pyidml/base.py
+++ pyidml/base.py
@@ -3,11 +3,14 @@ import os as os
 
 class pyIDML(object):
     # This class is used to hold and process IDML
-    def __init__(self, idmlfile=False):
+    def __init__(self, idml_file=False):
         # if idmlfile is provided will automatically parse idml file
-        if idmlfile:
-            self.fileName = idmlfile
-            self.parse()
+        if idml_file:
+            self.fileName = idml_file
+            if os.path.isfile(self.fileName):
+                self.parse()
+            else:
+                print("File does not exist: " + self.fileName)
         else:
             self.fileName = None
 
@@ -18,26 +21,26 @@ class pyIDML(object):
 
 class pyICML(object):
     # This class is used to hold and process ICML
-    def __init__(self, fileOrString=False):
+    def __init__(self, file_or_string=False):
         # fileOrString, upon creation can parse a file or string of ICML
-        if fileOrString:
-            if os.path.isfile(fileOrString):
-                self.fileName = fileOrString
+        if file_or_string:
+            if os.path.isfile(file_or_string):
+                self.fileName = file_or_string
                 self.parse_file()
             else:
                 self.fileName = None
-                self.parse_string(fileOrString)
+                self.parse_string(file_or_string)
         else:
             self.fileName = None
 
     def parse_file(self):
         print("Parsing " + self.fileName)
         with open(self.fileName, "rt") as icmlFile:
-            rawICML = icmlFile.read()
-        self.parse_string(rawICML)
+            raw_icml = icmlFile.read()
+        self.parse_string(raw_icml)
         pass
 
-    def parse_string(self, rawICML):
-        print("Parsing " + rawICML)
+    def parse_string(self, raw_icml):
+        print("Parsing " + raw_icml)
         pass
 
