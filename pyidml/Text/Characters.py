8e8ace32b49afbc11a7866b246157543a9e2893a
Josh Klos <me@joshklos.com>
2017-10-19 13:29:52 -0500

    Initial building of the Character class

--- pyidml/Text/Characters.py
+++ pyidml/Text/Characters.py
@@ -1,6 +1,17 @@
 class Character(object):
-    def __init__(self, content, charStyle, paraStyle, id):
+    """Class to contain the basic building block of a story, individual characters
+
+        Upon creation takes 5 arguments:
+        content: The actual character itself
+        char_style: The applied character style for this character (takes a string)
+        para_style: The applied paragraph style for this character's parent paragraph (takes a string)
+        id: An id number indicating the position of the character in a story
+        parent: The parent paragraph object
+    """
+    def __init__(self, content, char_style, para_style, id, parent):
         self.content = content
-        self.charStyle = charStyle
-        self.paraStyle = paraStyle
+        self.charStyle = char_style
+        self.paraStyle = para_style
         self.id = id
+        self.parentParagraph = parent
+        
